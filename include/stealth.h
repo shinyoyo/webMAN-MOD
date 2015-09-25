3a2b1ee786d8a1e394dc33658c649dbf293dda69
Matsumot0 <oueshgrout@free.fr>
2015-09-25 17:18:35 +0200

    some updates
    - Updated LANG_SK.TXT webMAN_SK.xml with the files provided by
    prometheus
    - Added syscall 38 to remove_cfw_syscalls() in stealth.h
    - lv2 peek/poke now are the last removals in remove_cfw_syscalls()
    
    Thanks @aldostools :+1:

--- include/stealth.h
+++ include/stealth.h
@@ -6,30 +6,29 @@ static void remove_cfw_syscalls(void)
 	if(c_firmware==0.00f) return;
 
     u64 sc_null = peekq(SYSCALL_TABLE);
+	u64 sc_not_impl_pt = peekq(sc_null);
 
 	get_idps_psid();
-	#ifndef COBRA_ONLY
-	pokeq(SYSCALL_PTR( 8), sc_null);
-	pokeq(SYSCALL_PTR(35), sc_null);
-	#endif
-	pokeq(SYSCALL_PTR( 9), sc_null);
-	pokeq(SYSCALL_PTR(10), sc_null);
-	pokeq(SYSCALL_PTR(11), sc_null);
-	pokeq(SYSCALL_PTR(36), sc_null);
-	pokeq(SYSCALL_PTR( 6), sc_null);
-	pokeq(SYSCALL_PTR( 7), sc_null);
 
-	u64 sc_not_impl_pt = peekq(sc_null);
+	#ifndef COBRA_ONLY
+	pokeq(SYSCALL_PTR( 8), sc_null); // lv1 peek / cobra syscall
+	pokeq(SYSCALL_PTR(35), sc_null); // Remapper PL3
+	#endif
+	pokeq(SYSCALL_PTR( 9), sc_null); // lv1 poke
+	pokeq(SYSCALL_PTR(10), sc_null); // lv2_lv1_call rebug
+	pokeq(SYSCALL_PTR(11), sc_null); // lv1 peek Hermes
+	pokeq(SYSCALL_PTR(36), sc_null); // Remapper
+	pokeq(SYSCALL_PTR(38), sc_null); // new sk1e syscall
+
 	#ifndef COBRA_ONLY
 	u64 sc8  = peekq(SYSCALL_PTR( 8));
 	u64 sc35 = peekq(SYSCALL_PTR(35));
 	#endif
-	u64 sc6  = peekq(SYSCALL_PTR( 6));
-	u64 sc7  = peekq(SYSCALL_PTR( 7));
 	u64 sc9  = peekq(SYSCALL_PTR( 9));
 	u64 sc10 = peekq(SYSCALL_PTR(10));
 	u64 sc11 = peekq(SYSCALL_PTR(11));
 	u64 sc36 = peekq(SYSCALL_PTR(36));
+	u64 sc38 = peekq(SYSCALL_PTR(38));
 
 	#ifndef COBRA_ONLY
 	if(sc8 !=sc_null) pokeq(sc8,  sc_not_impl_pt);
@@ -39,8 +38,7 @@ static void remove_cfw_syscalls(void)
 	if(sc10!=sc_null) pokeq(sc10, sc_not_impl_pt);
 	if(sc11!=sc_null) pokeq(sc11, sc_not_impl_pt);
 	if(sc36!=sc_null) pokeq(sc36, sc_not_impl_pt);
-	if(sc6 !=sc_null) pokeq(sc6,  sc_not_impl_pt);
-	if(sc7 !=sc_null) pokeq(sc7,  sc_not_impl_pt);
+	if(sc38!=sc_null) pokeq(sc38, sc_not_impl_pt);
 
 	#ifdef PS3MAPI
   //{ system_call_3(SC_COBRA_SYSCALL8, SYSCALL8_OPCODE_PS3MAPI, PS3MAPI_OPCODE_DISABLE_SYSCALL, 8); }
@@ -49,10 +47,20 @@ static void remove_cfw_syscalls(void)
 	{ system_call_3(SC_COBRA_SYSCALL8, SYSCALL8_OPCODE_PS3MAPI, PS3MAPI_OPCODE_DISABLE_SYSCALL, 11); }
   //{ system_call_3(SC_COBRA_SYSCALL8, SYSCALL8_OPCODE_PS3MAPI, PS3MAPI_OPCODE_DISABLE_SYSCALL, 35); }
 	{ system_call_3(SC_COBRA_SYSCALL8, SYSCALL8_OPCODE_PS3MAPI, PS3MAPI_OPCODE_DISABLE_SYSCALL, 36); }
+	{ system_call_3(SC_COBRA_SYSCALL8, SYSCALL8_OPCODE_PS3MAPI, PS3MAPI_OPCODE_DISABLE_SYSCALL, 38); }
 	{ system_call_3(SC_COBRA_SYSCALL8, SYSCALL8_OPCODE_PS3MAPI, PS3MAPI_OPCODE_DISABLE_SYSCALL, 6); }
 	{ system_call_3(SC_COBRA_SYSCALL8, SYSCALL8_OPCODE_PS3MAPI, PS3MAPI_OPCODE_DISABLE_SYSCALL, 7); }
 	{ system_call_3(SC_COBRA_SYSCALL8, SYSCALL8_OPCODE_PS3MAPI, PS3MAPI_OPCODE_PDISABLE_SYSCALL8, 1); }//Partial disable syscall8 (Keep cobra/mamba+ps3mapi features only)
 	#endif
+
+	pokeq(SYSCALL_PTR( 6), sc_null); // lv2 peek
+	pokeq(SYSCALL_PTR( 7), sc_null); // lv2 poke
+
+	u64 sc6  = peekq(SYSCALL_PTR( 6));
+	u64 sc7  = peekq(SYSCALL_PTR( 7));
+
+	if(sc6 !=sc_null) pokeq(sc6,  sc_not_impl_pt);
+	if(sc7 !=sc_null) pokeq(sc7,  sc_not_impl_pt);
 }
 #endif
 
